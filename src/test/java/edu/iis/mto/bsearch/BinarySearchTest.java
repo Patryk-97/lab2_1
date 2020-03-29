308157ae16fef3249f4c60b06ba51ccee3023d68
Patryk.Stepien <215886@edu.p.lodz.pl>
2020-03-29 22:23:39 +0200

    testy_v1 Added test case for search a key, which is in multiple element seq in first position

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -9,7 +9,11 @@ import org.junit.jupiter.api.Test;
 
 class BinarySearchTest {
 
-    private static final int[] oneElementSeq = {1};
+    private static int[] oneElementSeq = {1};
+    private static int[] multipleElementSeq = {10, 20, 30, 40, 50};
+    private static final int MULTIPLE_ELEMENT_SEQ_FIRST_POSITION = 0;
+    private static final int MULTIPLE_ELEMENT_SEQ_MIDDLE_POSITION = 2;
+    private static final int MULTIPLE_ELEMENT_SEQ_LAST_POSITION = 4;
     private BinarySearch binarySearch;
     private static final int NOT_FOUND = -1;
 
@@ -35,5 +39,15 @@ class BinarySearchTest {
         assertFalse(searchResult.isFound());
         int position = searchResult.getPosition();
         assertEquals(position, NOT_FOUND);
+    }
+
+    @Test
+    void multipleElementSeqWhichHasKeyInFirstPositionTest() {
+        int key = 10;
+        SearchResult searchResult = binarySearch.search(key, multipleElementSeq);
+        assertTrue(searchResult.isFound());
+        int position = searchResult.getPosition();
+        assertEquals(position, MULTIPLE_ELEMENT_SEQ_FIRST_POSITION);
+        assertEquals(key, multipleElementSeq[position]);
     }
 }
